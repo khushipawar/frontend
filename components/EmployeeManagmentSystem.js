9e95c7803c2fd179b4609b1e8b2ba92ed4ef0a64
Khushi Pawar <88932835+khushipawar@users.noreply.github.com>
2023-06-13 16:55:58 +0530

    Update EmployeeManagmentSystem.js

--- components/EmployeeManagmentSystem.js
+++ components/EmployeeManagmentSystem.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
-class EmployeeManagementSystem extends React.Component {
+class EmployeeManagementSystem extends Component {
   constructor(props) {
     super(props);
     this.state = {
       employees: [],
       name: '',
       email: '',
-      jobTitle: ''
+      jobTitle: '',
+      updateEmployeeId: null,
+      updatedEmployeeName: '',
+      updatedEmployeeEmail: '',
+      updatedEmployeeJobTitle: ''
     };
   }
 
@@ -30,6 +34,42 @@ class EmployeeManagementSystem extends React.Component {
     this.setState({ [event.target.name]: event.target.value });
   };
 
+  handleUpdateClick = (employeeId) => {
+    const { employees } = this.state;
+    const employeeToUpdate = employees.find(employee => employee.id === employeeId);
+    if (employeeToUpdate) {
+      this.setState({
+        updateEmployeeId: employeeToUpdate.id,
+        updatedEmployeeName: employeeToUpdate.name,
+        updatedEmployeeEmail: employeeToUpdate.email,
+        updatedEmployeeJobTitle: employeeToUpdate.jobTitle
+      });
+    }
+  };
+
+  handleSaveClick = () => {
+    const { updateEmployeeId, updatedEmployeeName, updatedEmployeeEmail, updatedEmployeeJobTitle } = this.state;
+    const updatedEmployee = {
+      name: updatedEmployeeName,
+      email: updatedEmployeeEmail,
+      jobTitle: updatedEmployeeJobTitle
+    };
+
+    axios.put(`http://localhost:8080/api/employees/${updateEmployeeId}`, updatedEmployee)
+      .then(response => {
+        this.fetchEmployees();
+        this.setState({
+          updateEmployeeId: null,
+          updatedEmployeeName: '',
+          updatedEmployeeEmail: '',
+          updatedEmployeeJobTitle: ''
+        });
+      })
+      .catch(error => {
+        console.error('Error updating employee:', error);
+      });
+  };
+
   handleAddEmployee = (event) => {
     event.preventDefault();
     const { name, email, jobTitle } = this.state;
@@ -40,7 +80,7 @@ class EmployeeManagementSystem extends React.Component {
       jobTitle: jobTitle
     };
 
-    axios.post('http://localhost:8080/employees/', newEmployee)
+    axios.post('http://localhost:8080/api/employees/', newEmployee)
       .then(response => {
         this.setState(prevState => ({
           employees: [...prevState.employees, response.data],
@@ -53,23 +93,9 @@ class EmployeeManagementSystem extends React.Component {
         console.error('Error adding employee:', error);
       });
   };
-  
-  handleUpdateEmployee = (id, updatedEmployee) => {
-    axios.put(`http://localhost:8080/employees/${id}`, updatedEmployee)
-      .then(response => {
-        this.setState(prevState => ({
-          employees: prevState.employees.map(employee =>
-            employee.id === id ? response.data : employee
-          )
-        }));
-      })
-      .catch(error => {
-        console.error('Error updating employee:', error);
-      });
-  };
 
   handleDeleteEmployee = (id) => {
-    axios.delete(`http://localhost:8080/employees/${id}`)
+    axios.delete(`http://localhost:8080/api/employees/${id}`)
       .then(() => {
         this.setState(prevState => ({
           employees: prevState.employees.filter(employee => employee.id !== id)
@@ -81,7 +107,7 @@ class EmployeeManagementSystem extends React.Component {
   };
 
   render() {
-    const { employees, name, email, jobTitle } = this.state;
+    const { employees, name, email, jobTitle, updateEmployeeId, updatedEmployeeName, updatedEmployeeEmail, updatedEmployeeJobTitle } = this.state;
 
     return (
       <div>
@@ -90,7 +116,7 @@ class EmployeeManagementSystem extends React.Component {
           {employees.map(employee => (
             <li key={employee.id}>
               {employee.name} - {employee.email} - {employee.jobTitle}
-              <button onClick={() => this.handleUpdateEmployee(employee.id, { name: 'Updated Name', email: 'Updated email', jobTitle: 'Updated jobTitle' })}>
+              <button onClick={() => this.handleUpdateClick(employee.id)}>
                 Update
               </button>
               <button onClick={() => this.handleDeleteEmployee(employee.id)}>
@@ -100,8 +126,7 @@ class EmployeeManagementSystem extends React.Component {
           ))}
         </ul>
         <h2>Add Employee</h2>
-        
-       <form onSubmit={this.handleAddEmployee}>
+        <form onSubmit={this.handleAddEmployee}>
           <input
             type="text"
             name="name"
@@ -114,19 +139,47 @@ class EmployeeManagementSystem extends React.Component {
             name="email"
             value={email}
             onChange={this.handleInputChange}
-            placeholder="email"
+            placeholder="Email"
           />
           <input
             type="text"
             name="jobTitle"
             value={jobTitle}
             onChange={this.handleInputChange}
-            placeholder="jobTitle"
+            placeholder="Job Title"
           />
           <button type="submit">Add Employee</button>
         </form>
 
-     
+        {updateEmployeeId && (
+          <div>
+            <h2>Update Employee</h2>
+            <form>
+              <input
+                type="text"
+                name="updatedEmployeeName"
+                value={updatedEmployeeName}
+                onChange={this.handleInputChange}
+                placeholder="Updated Name"
+              />
+              <input
+                type="text"
+                name="updatedEmployeeEmail"
+                value={updatedEmployeeEmail}
+                onChange={this.handleInputChange}
+                placeholder="Updated Email"
+              />
+              <input
+                type="text"
+                name="updatedEmployeeJobTitle"
+                value={updatedEmployeeJobTitle}
+                onChange={this.handleInputChange}
+                placeholder="Updated Job Title"
+              />
+              <button onClick={this.handleSaveClick}>Save</button>
+            </form>
+          </div>
+        )}
       </div>
     );
   }
